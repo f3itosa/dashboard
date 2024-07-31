ef8b7c960acf1b5e5031f4575ad1b129ffc7f005
Eric Feitosa <eric_feitosa13@hotmail.com>
2024-07-31 18:57:12 -0300

    update dashboard

---
+++ conftest.py
@@ -0,0 +1,14 @@
+import pytest
+import time
+from selenium import webdriver
+
+@pytest.fixture(scope="module")
+
+def browser():
+	chrome_options = webdriver.ChromeOptions()
+	
+	chrome_options.add_argument('--headless')
+	chrome_options.add_argument('--disable-gpu')
+	driver = webdriver.Chrome(options=chrome_options)
+	yield driver
+	driver.quit()
